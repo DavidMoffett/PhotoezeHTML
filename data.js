e6504c21b4558aa11cb5e89e78c25b57e343bdce
DavidMoffett <david@moffett.co.nz>
2026-04-24 12:35:47 +1200

    Switch PayPal to LIVE

--- data.js
+++ data.js
@@ -3,4 +3,4 @@ window.SUPABASE_URL = "https://oiozdxqxfujxkknngsxp.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pb3pkeHF4ZnVqeGtrbm5nc3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NDcyNDIsImV4cCI6MjA5MjMyMzI0Mn0.cp6q8bG0nEkVMPwz8wU-fTppTn4-NxPWeonURnbVyWA";
 
 // PayPal config
-window.PAYPAL_CLIENT_ID = "AfLMHq4YC9sgv4TPGhO-qwM8pQyoZ-Zo5j5Zw1TRmMhUKg75RR0WBxUIhySdRDoIorfUv2kkxyzT32lO";
+window.PAYPAL_CLIENT_ID = "AcK3ELcs-b-kVESn2LrWA42yNXsnT0Mj_LN8on39_hAr-BFMF_951w0OmVfV_DzbklGkHBPgkuxL5KvA";
